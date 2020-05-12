606e4d709a3863d0f5e4a5452dd3394ebe5427bb
Codycoder <andykohler99@gmail.com>
2020-05-11 22:15:21 -0400

    Test commit

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 // Make h1 in header have different text
+
+const thingy = document.querySelector("header h1#head")
+console.log(thingy)
+thingy.innerText = "Yup"
+
 // Make nav ul li elements get larger when you hover over them
+
+const listItems = document.querySelectorAll("li")
+console.log(listItems)
+
+// foreach(listItem.onmouseover)
+Here is a change
+
 // Add another element to the main element
+
+// const lines = document.querySelectorAll("#p")
+// addLines.innerText("Another Element")
+
+
 // Dynamically change the color of the background on the footer when the user hovers over it
+
 // Change the font size of the paragraphs in the main element when the user clicks them
+
 // Change the text in the footer when the user clicks it
+
 // Remove the h1 element in the header
+
+
+
 // Put something in the place of the h1 in the header
+
+
 // Change the text in one of the paragraph tags in the main element
+
+
 // Change the size of the header element to 200px using grid syntax
